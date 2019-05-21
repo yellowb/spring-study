749dc4159c47864f9bf2d976686d5ec4a98215c0
yellowb <122517167@qq.com>
2019-05-21 17:39:41 +0800

    add single data source jdbc demo

--- single-data-source-mysql/src/main/java/com/yellowb/singledatasourcemysql/controller/PersonController.java
+++ single-data-source-mysql/src/main/java/com/yellowb/singledatasourcemysql/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.yellowb.singledatasourcemysql.pojo.Person;
 import com.yellowb.singledatasourcemysql.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,8 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public Person getById() {
-        return personService.getById(1);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Person getById(@PathVariable("id") int id) {
+        return personService.getById(id);
     }
 }
