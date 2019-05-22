0f8d85720b4ca9712d67a6c007800695a4a69dc4
yellowb <122517167@qq.com>
2019-05-22 09:47:19 +0800

    add single data source jdbc demo

--- single-data-source-mysql/src/main/java/com/yellowb/singledatasourcemysql/service/impl/PersonServiceImpl.java
+++ single-data-source-mysql/src/main/java/com/yellowb/singledatasourcemysql/service/impl/PersonServiceImpl.java
@@ -3,6 +3,7 @@ package com.yellowb.singledatasourcemysql.service.impl;
 import com.yellowb.singledatasourcemysql.pojo.Person;
 import com.yellowb.singledatasourcemysql.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -24,7 +25,12 @@ public class PersonServiceImpl implements PersonService {
 
     @Override
     public Person getById(int id) {
-        return jdbcTemplate.queryForObject("select id, username from userinfo where id = ?", new Object[] {Integer.valueOf(id)},
-            new BeanPropertyRowMapper<>(Person.class));
+        try {
+            Person person = jdbcTemplate.queryForObject("select id, username from userinfo where id = ?", new Object[] {Integer.valueOf(id)},
+                new BeanPropertyRowMapper<>(Person.class));
+            return person;
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 }
