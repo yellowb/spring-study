639ad5ae4d80ae55ca0c2cf17acc2200314b6e30
yellowb <122517167@qq.com>
2019-05-24 13:43:31 +0800

    add single data source mybatis demo

---
+++ single-data-source-mybatis-mysql/src/main/java/com/yellowb/singledatasourcemybatismysql/SingleDataSourceMybatisMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.yellowb.singledatasourcemybatismysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleDataSourceMybatisMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleDataSourceMybatisMysqlApplication.class, args);
+	}
+
+}
--- single-data-source-mybatis-mysql/src/test/java/com/yellowb/singledatasourcemybatismysql/SingleDataSourceMybatisMysqlApplicationTests.java
+++ single-data-source-mybatis-mysql/src/test/java/com/yellowb/singledatasourcemybatismysql/SingleDataSourceMybatisMysqlApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yb.demo_helloworld;
+package com.yellowb.singledatasourcemybatismysql;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoHelloworldApplicationTests {
+public class SingleDataSourceMybatisMysqlApplicationTests {
 
 	@Test
 	public void contextLoads() {
